c8f97b4347e215b4ec21bd6d8d7bd3ae6de74e60
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-09-11 17:31:54 -0600

    Adds app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+new Vue({
+  el: '#app',
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false
+  },
+  methods: {
+    startGame: function() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    }
+  }
+})
+
+
+
+
+
+
+
+// health
+
+// Start new game
+
+// Attack
+
+// Special Attack
+
+// Heal
+
+// Give Up
