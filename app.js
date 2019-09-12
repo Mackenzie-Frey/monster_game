03e27a16f62c8e8f7ff96c51302a7f92b6e74cf7
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-09-11 18:43:53 -0600

    Functional Attack function which restarts new game.

--- app.js
+++ app.js
@@ -10,6 +10,49 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack: function() {
+      var damage = this.calculateDamage(3,10)
+      this.monsterHealth -= damage;
+
+      if (this.checkWin()) {
+        return;
+      }
+
+      var damage = this.calculateDamage(5,12)
+      this.playerHealth -= damage;
+
+      this.checkWin();
+    },
+    calculateDamage: function(min,max) {
+      return Math.max(Math.floor(Math.random() * max) + 1);
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You won! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return;
+      } else if (this.playerHealth <= 0) {
+          if (confirm('You lost! New Game?')) {
+            this.startGame();
+          } else {
+            this.gameIsRunning = false;
+          }
+          return true;
+        }
+        return false;
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
     }
   }
 })
